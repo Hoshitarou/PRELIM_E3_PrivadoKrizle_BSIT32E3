8b0f9270121b88675f41174bf0de29e51942aa35
Ampong_JemichoLouise_BSIT32E2 <160830705+AmpongJemichoLouiseBSIT@users.noreply.github.com>
2024-03-02 14:47:01 +0800

    Initial commit
    Removed zip file

---
+++ PRELIM_E3_Assignment_PrivadoKrizle_BSIT32E1/PRELIM_E3_Assignment_PrivadoKrizle_BSIT32E1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRELIM_E3_Assignment_PrivadoKrizle_BSIT32E1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
